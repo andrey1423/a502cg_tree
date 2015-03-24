a0d3442c14eef7589e3be0df632bd40e8d50eb49
andrey1423 <andrey.200249@gmail.com>
2015-03-24 15:21:12 +0300

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/a502cg.mk \
+    $(LOCAL_DIR)/omni_a502cg.mk
---
+++ omni_a502cg.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/a502cg/a502cg.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a502cg
+PRODUCT_NAME := omni_a502cg
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := a502cg
+PRODUCT_MANUFACTURER := asus
